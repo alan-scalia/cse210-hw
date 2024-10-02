b863806f10b837fa18e2ef48ff4f7b12e839603b
Alan Scalia <alan.sca07@gmail.com>
2024-10-02 08:22:08 -0300

    Learning 03 Fractions activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Write("Enter the top number: ");
+        int topNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter the bottom number: ");
+        int bottomNumber = int.Parse(Console.ReadLine());
+
+        Fraction defaultFraction = new Fraction();
+        Fraction topFraction = new Fraction(topNumber);
+        Fraction classicFraction = new Fraction(topNumber, bottomNumber);
+
+        Console.WriteLine(defaultFraction.GetFractionString());
+        Console.WriteLine(topFraction.GetFractionString());
+        Console.WriteLine(classicFraction.GetFractionString());
+
+        Console.WriteLine(defaultFraction.GetDecimalValue());
+        Console.WriteLine(topFraction.GetDecimalValue());
+        Console.WriteLine(classicFraction.GetDecimalValue());
     }
 }
